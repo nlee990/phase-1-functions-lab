d0f07c7ae68b762e0dfd177239f6afc26ddecd13
Neomi Lee <neomilee1@gmail.com>
2022-12-15 22:05:54 -0700

    commit tree 08f3a6ffd257f060b69521f6d5260c0a4c76a642

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    var dist;
+    if (pickUp > 42) {
+        dist = pickUp - 42;
+    } 
+    else {
+        dist = 42 - pickUp;
+    }
+    return dist;
+} 
+function distanceFromHqInFeet(pickUp) {
+    var dist;
+    if (pickUp > 42) {
+        dist = pickUp - 42;
+    } 
+    else {
+        dist = 42 - pickUp;
+    }
+
+    return dist * 264;
+} 
+function distanceTravelledInFeet(pickUp, dropOff) {
+    var dist;
+    if (pickUp > dropOff) {
+        dist = pickUp - dropOff;
+    } 
+    else {
+        dist = dropOff - pickUp;
+    }
+    return dist * 264;
+}
+function calculatesFarePrice(pickUp, dropOff) {
+    var price = 0;
+    var dist = Math.abs(pickUp - dropOff) * 264;
+
+    if (dist > 400 && dist < 2000){
+        dist -= 400
+        price = dist * 0.02;
+    } 
+    else if (dist > 2000 && dist < 2500) {
+        price = 25;
+    } else if (dist > 2500){
+        price = 'cannot travel that far';
+    }
+    return price;
+
+    
+}
